c590181a71fb29243a067f0f18ce9c6841619ce8
Ishu <kashyapishwar52@gmail.com>
2023-12-27 01:25:15 +0530

    Create FactorialCalculation.js
    In this function, the base case is when 'n' is 0, in which the factorial is 1. In the recursive case, the function calculates the factorial by multiplying 'n' with the factorial of '(n - 1)'. The recursion continues until the base case is reached.

---
+++ FactorialCalculation.js
@@ -0,0 +1,18 @@
+function factorial(n) 
+{
+
+  // Base case: factorial of 0 is 1
+  
+  if (n === 0) {
+    return 1;
+  }
+
+  // Recursive case: n! = n * (n-1)!
+  
+  return n * factorial(n - 1);
+}
+
+// Example usage:
+
+const number = 5;
+console.log(`The factorial of ${number} is: ${factorial(number)}`);
